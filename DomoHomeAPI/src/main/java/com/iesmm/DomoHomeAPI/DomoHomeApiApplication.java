2012f2dbdbdca456d619c4b6b48b54f39fa372b2
fsalgon813 <91082273+fsalgon813@users.noreply.github.com>
2023-04-18 15:50:56 +0200

    Add files via upload

---
+++ DomoHomeAPI/src/main/java/com/iesmm/DomoHomeAPI/DomoHomeApiApplication.java
@@ -0,0 +1,13 @@
+package com.iesmm.DomoHomeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomoHomeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomoHomeApiApplication.class, args);
+	}
+
+}
---
+++ DomoHomeAPI/src/test/java/com/iesmm/DomoHomeAPI/DomoHomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesmm.DomoHomeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomoHomeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
